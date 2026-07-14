cea696121657799e58d2b15aa81bd9f18bbaf3fe
Lewin Kelly <heli@odex.cf>
2026-07-14 21:15:57 +0100

    Allow invocation from both client and server side

--- shared/shared.go
+++ shared/shared.go
@@ -1,5 +1,12 @@
 package shared
 
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"net/http"
+)
+
 type Data interface {
 	Validate() error
 }
@@ -7,4 +14,37 @@ type Data interface {
 type RemoteFunc[ReqType Data, ResType Data] struct {
 	Name     string
 	Callback func(ReqType) ResType
+}
+
+const url = "http://localhost:8080"
+
+func (f RemoteFunc[ReqType, ResType]) invokeServer(arg ReqType) (res ResType, err error) {
+	var w bytes.Buffer
+	if err = gob.NewEncoder(&w).Encode(arg); err != nil {
+		return res, fmt.Errorf("failed to encode request: %w", err)
+	}
+
+	hres, err := http.Post(url+"/api/"+f.Name, "application/octet-stream", &w)
+	if err != nil {
+		return res, fmt.Errorf("failed to send request: %w", err)
+	}
+	defer hres.Body.Close()
+
+	if hres.StatusCode != http.StatusOK {
+		return res, fmt.Errorf("server returned bad status: %s", hres.Status)
+	}
+
+	if err = gob.NewDecoder(hres.Body).Decode(&res); err != nil {
+		return res, fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	return
+}
+
+func (f RemoteFunc[ReqType, ResType]) Invoke(arg ReqType) (ResType, error) {
+	if f.Callback != nil {
+		return f.Callback(arg), nil
+	}
+
+	return f.invokeServer(arg)
 }
